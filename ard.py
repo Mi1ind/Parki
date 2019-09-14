c7b5bb659e260b7978fc53431ecf9c6f9b5d54f3
Tapish Jian <tapishleafs81@gmail.com>
2019-09-14 16:06:59 -0400

    Update ard.py

--- ard.py
+++ ard.py
@@ -1,10 +1,13 @@
 import serial
 
 arduinoSerialData = serial.Serial('/dev/cu.usbmodem14201', 9600)
-
+n=200
 while (1 == 1):
     if (arduinoSerialData.inWaiting() > 0):
         mydata = arduinoSerialData.readline()
         # print((mydata[8:11]))
         print(len(mydata))
-# while (float(mydata) < 2)
+if (float(mydata) == 13){
+n+=-1;
+}
+
