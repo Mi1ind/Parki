c44a2bd3ec593c94a8b02788296ebc25086321e6
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2019-09-14 15:23:48 -0400

    1000 IQ solution

--- ard.py
+++ ard.py
@@ -1,9 +1,10 @@
 import serial
 
-arduinoSerialData = serial.Serial('/dev/cu.usbmodem14101', 9600)
+arduinoSerialData = serial.Serial('/dev/cu.usbmodem14201', 9600)
 
 while (1 == 1):
     if (arduinoSerialData.inWaiting() > 0):
         mydata = arduinoSerialData.readline()
-        print(mydata)
+        # print((mydata[8:11]))
+        print(len(mydata))
 # while (float(mydata) < 2)
