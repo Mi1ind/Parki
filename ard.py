d7c9a3de05a617511c915a63ff7cdd08d66b2289
Nabil Mohamed <n.mohamed135@gmail.com>
2019-09-14 17:35:54 -0400

    car park detection mechanic
    added core feature : with the ultrasonic sensor, our system can now detect when cars enter a parking lot, and use that to update information about available parking lot spaces.

--- ard.py
+++ ard.py
@@ -1,10 +1,27 @@
 import serial
 
-arduinoSerialData = serial.Serial('/dev/cu.usbmodem14201', 9600)
+car_has_entered = False
+num_cars = 0
+total_spaces = 200
+
+# arduinoSerialData = serial.Serial('/dev/cu.usbmodem14201', 9600)
+arduinoSerialData = serial.Serial('COM5', 9600)
+
 
 while (1 == 1):
     if (arduinoSerialData.inWaiting() > 0):
         mydata = arduinoSerialData.readline()
-        # print((mydata[8:11]))
-        print(len(mydata))
-# while (float(mydata) < 2)
+        currDistance = len(mydata)
+
+        if(currDistance == 13):
+            car_has_entered = True
+
+        if(currDistance > 13):
+            if(car_has_entered):
+                num_cars += 1
+                #print("NUMBER OF CARS IS NOW: ", num_cars)
+                print("THERE ARE : ", total_spaces - num_cars, " SPACES LEFT")
+                car_has_entered = False
+    
+        #print(mydata)
+        #print(currDistance)
--- sketch_sep14a/sketch_sep14a.ino
+++ sketch_sep14a/sketch_sep14a.ino
@@ -26,7 +26,7 @@ void loop()
   //This gives us distance in cm
   distance = duration * 0.034 / 2;
 
-  Serial.println("Distance: ");
+  //Serial.println("Distance: ");
   Serial.println(distance);
 
   //Millisecond
