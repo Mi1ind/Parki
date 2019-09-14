88549899b42538cd418c57e60c61b7f47bdab77e
Milind Sai Waghrey <m.sai.waghrey@gmail.com>
2019-09-14 13:08:11 -0400

    some changes

--- sketch_sep14a/sketch_sep14a.ino
+++ sketch_sep14a/sketch_sep14a.ino
@@ -4,14 +4,16 @@ const int trigPin = 8;
 long duration;
 long distance;
 
-void setup() {
+void setup()
+{
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
-  digitalWrite(trigPin, LOW); 
+void loop()
+{
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(10);
 
   digitalWrite(trigPin, HIGH);
@@ -22,7 +24,7 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
 
   //This gives us distance in cm
-  distance = duration*0.034/2;
+  distance = duration * 0.034 / 2;
 
   Serial.println("Distance: ");
   Serial.println(distance);
