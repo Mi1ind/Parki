c3c613e5fd6e2a1fcf9a68ef5935d1be91cf6b60
Nabil Mohamed <n.mohamed135@gmail.com>
2019-09-15 08:56:53 -0400

    Update ard.py

--- ard.py
+++ ard.py
@@ -1,28 +1,16 @@
 import serial
 
-"""
-#old setup for only single arduino
-car_has_entered = False
-num_cars = 0
-total_spaces = 200
-"""
-#arduinoSerialData = serial.Serial('/dev/cu.usbmodem14201', 9600)
-
-#arduinoSerialData = serial.Serial('COM5', 9600)
-
-
 class ParkingLot:
     def __init__(self, port, br, lots, name):
 
-        
-        #initializes the properties of a new parking lot object.
-        #parameters: serial port (i.e. 'COM5'), baudrate, max # of parking spaces
-        #Example: simon_street = ParkingLot('COM2', 9600, 20)
-        
+        """
+        initializes the properties of a new parking lot object.
+        parameters: serial port (i.e. 'COM5'), baudrate, max # of parking spaces
+        Example: simon_street = ParkingLot('COM2', 9600, 20)
+        """
         
         self.data_stream = serial.Serial(port, br)
         self.num_parking_lots = lots
-        self.currDistance = 20
 
         self.num_cars = 0
         self.has_entered = False
@@ -31,14 +19,19 @@ class ParkingLot:
     def update(self):
         if(self.data_stream.inWaiting() > 0):
             self.currDistance = len(self.data_stream.readline())
-            if(self.currDistance < 15):
+            if(self.currDistance == 13):
                 self.has_entered = True
+            
+            elif(self.num_cars == self.num_parking_lots):
+                return 0
                 
             elif(self.currDistance == 15):
                 if(self.has_entered):
                     self.num_cars += 1
                     print(self.name, ": THERE ARE: ", max(0,self.num_parking_lots - self.num_cars), " SPACES LEFT")
                     self.has_entered = False
+     
+        
                     
 bay_street = ParkingLot('COM5', 9600, 20, 'bay street') #20 parking spaces
 osgoode_lane = ParkingLot('COM6', 9600, 50, 'osgoode lane') #50 parking spaces
